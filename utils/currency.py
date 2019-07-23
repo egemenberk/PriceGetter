b4e939ae46eca25b50ff5c8efefd2ce96b591949
axydor <e2099018@ceng.metu.edu.tr>
2019-07-23 20:07:14 +0000

    Currency symbol is now only checked for USD

--- utils/currency.py
+++ utils/currency.py
@@ -1,26 +1,11 @@
 
-TL = [  "vatanbilgisayar.com",
-        "hepsiburada.com",
-        "qp.com.tr",
-        "amazon.com.tr",
-        "ebrarbilgisayar.com",
-        "incehesap.com",
-        "trendyol.com",
-        "itopya.com",
-        "sinerji.gen.tr",
-        "gameekstra.com",
-        "urun.n11.com"
-        ]
-
 USD = [ "amazon.com",
         "ebay.com",
         "newegg.com",
 ]
 
 def get_currency(url):
-    if url in TL:
-        return "₺"
-    elif url in USD:
+    if url in USD:
         return "$"
     else:
-        return "#"
+        return "₺"
